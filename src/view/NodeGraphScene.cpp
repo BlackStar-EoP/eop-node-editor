b3e00cffd8e8e0e47503a9d953e7d8dd74bd331e
BlackStar <tribalrage@gmail.com>
2018-03-06 13:43:27 +0100

    support for grid lines

--- src/view/NodeGraphScene.cpp
+++ src/view/NodeGraphScene.cpp
@@ -75,3 +75,22 @@ void NodeGraphScene::mouseReleaseEvent(QGraphicsSceneMouseEvent* event)
 	m_line_edit_item = nullptr;
 	QGraphicsScene::mouseReleaseEvent(event);
 }
+
+void NodeGraphScene::drawBackground(QPainter* painter, const QRectF& rect)
+{
+    const int gridSize = 25;
+
+    qreal left = int(rect.left()) - (int(rect.left()) % gridSize);
+    qreal top = int(rect.top()) - (int(rect.top()) % gridSize);
+
+    QVarLengthArray<QLineF, 100> lines;
+
+    for (qreal x = left; x < rect.right(); x += gridSize)
+        lines.append(QLineF(x, rect.top(), x, rect.bottom()));
+    for (qreal y = top; y < rect.bottom(); y += gridSize)
+        lines.append(QLineF(rect.left(), y, rect.right(), y));
+
+    qDebug() << lines.size();
+
+    painter->drawLines(lines.data(), lines.size());
+}
